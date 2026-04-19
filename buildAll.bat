b73ef84b6fe7ddd48e44d12011af100027d128df
yitenchen123 <2916782430@qq.com>
2026-04-19 17:27:58 +0800

    Add files via upload

---
+++ buildAll.bat
@@ -0,0 +1,27 @@
+@echo off & setlocal enabledelayedexpansion
+@rem Note for devs: If this script doesnt work, please look at the unix buildAll script
+@rem This script was originally created on linux, so there may be some problems with this translation
+
+
+echo ==================== Note: All build jars will be in the folder called 'buildAllJars' ====================
+mkdir buildAllJars
+del buildAllJars/*
+del build/forgix/*
+
+@rem Loop trough everything in the version properties folder
+for %%f in (versionProperties\*) do (
+    @rem Get the name of the version that is going to be compiled
+    set version=%%~nf
+
+    @rem Clean out the folders, build it, and merge it
+    echo ==================== Cleaning workspace to build !version! ====================
+    call .\gradlew.bat clean
+    
+	echo ==================== Building !version! ====================
+    call .\gradlew.bat build -PmcVer="!version!"
+    
+    echo ==================== Moving jar ====================
+    move build\forgix\*.jar buildAllJars\
+)
+
+endlocal
